5b123307ab94cf16eb2bfdaa570f00c8cc62ae05
Mickeywaugh <30330842+Mickeywaugh@users.noreply.github.com>
2020-02-02 20:06:40 +0800

    Update Redis.php
    //增加构造函数，在创建redis对象时直接传入配置数组即可返回Redis对象
    调用例子  
    $redisConfig=["host"=>"127.0.0.1","port"=>"6379","password"=>"password123","timeout"=>0];
    $redis=new Redis($redisConfig);

--- src/api/struct/Redis.php
+++ src/api/struct/Redis.php
@@ -13,4 +13,11 @@ class Redis
     public $port = '6379';
     public $password = '';
     public $timeout = 0;
+    //增加构造函数，在创建redis对象时直接传入配置数组即可返回Redis对象
+    public function __construct($config=[]){
+		$this->host=$config['host']??$this->host;
+		$this->port=$config['port']??$this->port;
+		$this->password=$config['password']??$this->password;
+		$this->timeout=$config['timeout']??$this->timeout;
+	}
 }
